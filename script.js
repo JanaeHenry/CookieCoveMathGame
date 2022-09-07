930d8670e33eea61342ecedba883a52d9447c878
Janae Henry <janaedhenry@gmail.com>
2022-09-07 01:24:02 -0400

    style changes

--- script.js
+++ script.js
@@ -24,7 +24,31 @@ let displayMath = document.getElementById('equation');
 let gameInput = document.getElementById('game-input');
 const submitBtn = document.getElementById('submit-btn');
 const startButton = document.getElementById('start-btn');
+
+// const redHearts = document.getElementsByClassName('redhearts');
+
+// const greyHearts = document.getElementsByClassName('greyhearts');
+
+// const redHeart1 = document.getElementById('redheart1');
+// const redHeart2 = document.getElementById('redheart2');
+// const redHeart3 = document.getElementById('redheart3');
+// const greyHeart1 = document.getElementById('greyheart1');
+// const greyHeart2 = document.getElementById('greyheart2');
+// const greyHeart3 = document.getElementById('greyheart3');
+
+const gameBoard = document.getElementById('game-board');
+
+let lose = document.getElementById('lose');
+let win = document.getElementById('win');
+
 let mathEquation;
+
+let hearts = [1, 2, 3];
+
+
+
+
+
 
 class Player { 
   constructor(health = 3, correct = 0) {
@@ -52,24 +76,30 @@ const submitAnswer = () => {
           
   // gameInput.value = '';
   let playerInput = parseInt(gameInput.value);
+
+
+
+
   
-  if((playerInput !== mathEquation) && (Player1.health === 1)){
-    Player1.health -= 1;
-    redHeart1.style.visibility = 'hidden';
-    greyHeart1.style.visibility = 'visible';
-    endGame()
+
+  
+  if((playerInput !== mathEquation) && (hearts.length === 1)){
+    hearts.pop();
+    // redHeart1.style.visibility = 'hidden';
+    // greyHeart1.style.visibility = 'visible';
+    hearts.length ? 0 : endGame();
     console.log('health = 1')
-  } else if ((playerInput !== mathEquation) && (Player1.health === 2)){
-    Player1.health -= 1;
-    redHeart2.style.visibility = 'hidden';
-    greyHeart2.style.visibility = 'visible';
+  } else if ((playerInput !== mathEquation) && (hearts.length === 2)){
+    hearts.pop();
+    // redHeart2.style.visibility = 'hidden';
+    // greyHeart2.style.visibility = 'visible';
     alert('wrong answer')
     runGame();
     console.log('health = 2')
-  } else if ((playerInput !== mathEquation) && (Player1.health === 3)){
-    Player1.health -= 1;
-    redHeart3.style.visibility = 'hidden';
-    greyHeart3.style.visibility = 'visible';
+  } else if ((playerInput !== mathEquation) && (hearts.length === 3)){
+    hearts.pop();
+    // redHeart3.style.visibility = 'hidden';
+    // greyHeart3.style.visibility = 'visible';
     alert('wrong answer')
     runGame();
     console.log('health = 3')
@@ -77,19 +107,21 @@ const submitAnswer = () => {
     Player1.correct++;
     runGame();
     console.log('health = 4')
-  } else if ((playerInput === mathEquation) && (Player1.correct === 7)){
+  } else if ((playerInput === mathEquation) && (Player1.correct >= 6)){ // change so that hooray message alerts immediately after 7th correct answer is entered
     winGame();
     console.log('health = 5')
 
-  } else if (Player1.health === 0) {
+  } else if (hearts.length == 0 ) {
     endGame();
   }
  setupQuestion();
  gameInput.value = '';
+ console.log(Player1.correct)
+ console.log(hearts.length)
 
   }
 
-console.log(Player1.health)
+
 
 
 submitBtn.addEventListener('click', () => {
@@ -111,20 +143,11 @@ submitBtn.addEventListener('click', () => {
 
 
 
-const redHearts = document.getElementsByClassName('redhearts');
 
-const greyHearts = document.getElementsByClassName('greyhearts');
-
-const redHeart1 = document.getElementById('redheart1');
-const redHeart2 = document.getElementById('redheart2');
-const redHeart3 = document.getElementById('redheart3');
-const greyHeart1 = document.getElementById('greyheart1');
-const greyHeart2 = document.getElementById('greyheart2');
-const greyHeart3 = document.getElementById('greyheart3');
 
 
 window.onload = () => {
-  document.getElementById('game-board').style.visibility = 'visible';
+  gameBoard.style.visibility = 'visible';
   startButton.style.visibility = 'visible';
   displayMath.style.visibility = 'hidden';
   gameInput.style.visibility = 'hidden';
@@ -150,22 +173,22 @@ function runGame() {
 
 function endGame() {
   // preventDefault();
-  let lose = document.getElementById('lose');
-  lose.style.display = 'block';
-  restart.style.display = 'block';
-  alert('Try again');
+  
+  // lose.style.display = 'block';
+  // restart.style.display = 'block';
+  alert('Restart game and try again');
 
 }
  
 function winGame() {
   // preventDefault();
-  let win = document.getElementById('win');
-  win.style.display = 'block';
-  restart.style.display = 'block';
-  alert('Hooray');
+  
+  // win.style.display = 'block';
+  // restart.style.display = 'block';
+  alert("Hooray! You've completed the challenge and saved Cookie Cove!");
 }
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('mouseup', startGame);
 
 
 
